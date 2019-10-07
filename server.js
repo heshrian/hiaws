34b5c36d869765f6557a8cf0543b6cd72cb3aae9
heshrian <kamburovviktor@gmail.com>
2019-10-07 15:25:43 +0200

    add() index.html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3333;
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello all')
+	res.sendFile('/views/index.html', {root:__dirname})
 });
 
 app.listen(PORT, () => {
