6a3647cca6912c9466f3cb451d14f157a48149ad
heshrian <kamburovviktor@gmail.com>
2019-10-21 16:47:39 +0200

    edit() add build_number

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 aws s3 cp ./Dockerrun.aws.json \
-  s3://elasticbeanstalk-eu-west-2-124429370407/hiDockerAgain/Dockerrun.aws.json
+  s3://elasticbeanstalk-eu-west-2-124429370407/hiDockerAgain/$BUILD_ID/Dockerrun.aws.json
 
 aws elasticbeanstalk create-application-version \
   --application-name "hiDockerAgain" \
   --version-label hiDockerAgain-$BUILD_ID \
-  --source-bundle S3Bucket="elasticbeanstalk-eu-west-2-124429370407",S3Key="hiDockerAgain/Dockerrun.aws.json" \
+  --source-bundle S3Bucket="elasticbeanstalk-eu-west-2-124429370407",S3Key="hiDockerAgain/$BUILD_ID/Dockerrun.aws.json" \
   --auto-create-application
 
   aws elasticbeanstalk update-environment \
   --application-name "hiDockerAgain" \
   --environment-name "hiDockerAgain-dev" \
-  --version-label hiDockerAgain \
+  --version-label hiDockerAgain-$BUILD_ID \
   
